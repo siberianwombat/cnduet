06cceff978fe0cb79d2b76ae3b79cd32d868454e
zav <a.w.zakharenko@gmail.com>
2017-09-14 13:54:26 +0300

    showing index page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // the main idea: get the cards position determined by prng seed given as the parameter
 // showing the first player his cars and giving him opportynity to send out the link to another part
+console.log("initializing the script")
 
 function Random(seed) {
     this._seed = seed % 2147483647;
@@ -18,12 +19,18 @@ function getRandomSeed() {
     return Math.round(Math.random()*2147483646);
 }
 
-// getting url parameters
-let docURL = new URL(document.location.href);
-let seed = docURL.searchParams('seed');
-let side = docURL.searchParams('side');
-// if no seed, offer to get one
-if (seed == null) {
-    return
-}
-// otherwise, show the panel corresponding to the side (player1/player2)
+(function (){
+    // getting url parameters
+    let docURL = new URL(document.location.href);
+    let seed = docURL.searchParams.get('seed');
+    let side = docURL.searchParams.get('side');
+    // if no seed, offer to get one
+    if (seed == null) {
+        // showing just index page
+        console.log("showing the index page")
+        document.getElementById("index").style.display='block';
+        document.getElementById("genLayoutLink").href = '?seed=' + getRandomSeed()
+        return
+    }
+    // otherwise, show the panel corresponding to the side (player1/player2)
+})();
